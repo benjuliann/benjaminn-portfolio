e9973fc94acc5426fb871619c020062e2c6787b0
launchland <benjulian.noel@gmail.com>
2026-01-16 04:34:37 -0700

    update divs

--- app/page.js
+++ app/page.js
@@ -4,11 +4,12 @@ import Image from "next/image";
 export default function Home() {
     return (
         
-        <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 dark:bg-gray-950 p-10 dark:text-white">
-
+        <main className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 dark:bg-gray-950 p-10 dark:text-white">
+            <nav className="flex gap-6 text-sm uppercase tracking-wide">nav</nav>
             <header className="mb-10">
                 <h2 className="text-2xl">welcome.</h2>
             </header>
+                 
             <h1 className="text-5xl font-bold tracking-tight">hi, i am <span className="bg-blue-400/70">benjamin</span> julian noel</h1>
             <p className="mt-4 text-lg">a passionate developer and music enthusiast. work in progress.</p>
             <Image
@@ -26,10 +27,10 @@ export default function Home() {
                 <a href="https://benjulian.carrd.co/"className="underline bg-blue-400/25 hover:bg-blue-400/70 transition-all duration-200 hover:scale-105 hover:font-bold px-1 rounded-sm">etc.</a>
             </div>
 
-            <footer className="mt-16 text-sm text-zinc-500">
+            <footer className="mt-16 text-sm text-zinc-500 ">
                 last updated: jan 2026. <br/>
                 © 2026 benjulian noel.
             </footer>
-        </div>
+        </main>
     );
 }
