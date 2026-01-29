aba3b6ca440e21ff98fadc5874c723748bc88e5e
launchland <benjulian.noel@gmail.com>
2026-01-29 01:24:15 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,20 +5,29 @@ export default function Home() {
     return (
         
         <main className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 dark:bg-gray-950 p-10 dark:text-white">
-            <nav className="flex gap-6 text-sm uppercase tracking-wide">nav</nav>
-            <header className="mb-10">
-                <h2 className="text-2xl">welcome.</h2>
+            <header>
+                <nav className="flex gap-6 text-sm uppercase tracking-wide ">
+                    <a href="#about" className="underline bg-blue-400/25 hover:bg-blue-400/70 transition-all duration-200 hover:scale-105 hover:font-bold px-1 rounded-sm">about.</a>
+                    <a href="#projects" className="underline bg-blue-400/25 hover:bg-blue-400/70 transition-all duration-200 hover:scale-105 hover:font-bold px-1 rounded-sm">projects.</a>
+                    <a href="#contact" className="underline bg-blue-400/25 hover:bg-blue-400/70 transition-all duration-200 hover:scale-105 hover:font-bold px-1 rounded-sm">contact.</a>
+                </nav>
             </header>
-                 
-            <h1 className="text-5xl font-bold tracking-tight">hi, i am <span className="bg-blue-400/70">benjamin</span> julian noel</h1>
-            <p className="mt-4 text-lg">a passionate developer and music enthusiast. work in progress.</p>
-            <Image
+            <div className="flex ">
+                <Image
                 src="/images/selfie.jpg"
                 alt="benjie's selfie"
-                width={450}
+                width={300}
                 height={300}
                 className="filter brightness-105 saturate-120 mt-6"
-            />
+                />
+                <div>
+                    <h4 className="text-5xl font-bold tracking-tight">Hello! I am Benjamin Julian Noel</h4>
+                    <p className="mt-2">Calgary, Canada</p>
+                    <p className="mt-4 text-lg">Full-stack Software Developer, Music Enthusiast, Software Development Diploma at Southern Alberta Institute of Technology</p>
+                </div>
+            </div>
+
+            
             <div className="mt-6 flex space-x-4 text-xl h-[2.5rem] items-center">
                 <a href="https://github.com/benjuliann"className="underline bg-blue-400/25 hover:bg-blue-400/70 transition-all duration-200 hover:scale-105 hover:font-bold px-1 rounded-sm">github.</a>
                 <a href="https://www.linkedin.com/in/benjuliannoel/"className="underline bg-blue-400/25 hover:bg-blue-400/70 transition-all duration-200 hover:scale-105 hover:font-bold px-1 rounded-sm">linkedin.</a>
