20e61b7e61089b09b7932c6e57e40a96a8b675e9
launchland <benjulian.noel@gmail.com>
2026-05-25 02:57:09 -0600

    updated home description

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
         <main className="max-w-4xl mx-auto flex flex-col min-h-screen justify-center bg-zinc-50 dark:bg-gray-950 p-10 dark:text-white">
                  
             <header className="text-5xl font-bold tracking-tight">Benjamin Julian Noel</header>
-            <h1 className="text-lg font-semibold mt-4 uppercase">Software Engineer / Producer / Student @ SAIT</h1>
-            <p className="mt-4 text-md">Hi there! I'm Benjamin, currently a student finishing up at SAIT. My work mainly focuses on full-stack programming, audio technology, and visual design. I combine these disciplines to create web-applications and projects that are robust and visually compelling.</p>
+            <h1 className="text-lg font-semibold mt-4 uppercase">Software Engineer / Producer / SAIT Graduate</h1>
+            <p className="mt-4 text-md">Hi there! I'm Benjamin, a software engineer, producer, and creative based in Calgary. Graduating from SAIT and incoming student at ULeth in 2026. My work lives at the intersection of full-stack development, cloud infrastructure, and visual design. Outside of code, I produce music and perform live, and was even brought back by my high school as an audio engineering resource. Most recently, my team placed 2nd at MegaHacks 2026.</p>
 
             <footer className="mt-16 text-sm text-zinc-500 items-center">
                 last updated: {new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} <br/>
